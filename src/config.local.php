f7a06b141f020a10b3bdb33f7d95397f14a3da04
sergiuko <termenskysergiy@gmail.com>
2026-03-03 16:31:22 +0100

    Add config.local.php with production-safe app/media defaults

---
+++ src/config.local.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'app' => [
+        'debug' => false,
+    ],
+    'media' => [
+        'storage_dir' => __DIR__ . '/../public/uploads',
+        'public_url_prefix' => '/uploads',
+    ],
+];

c3fb12994a2065fcf7dbe95ce9adf72e0ad5c76b
sergiuko <termenskysergiy@gmail.com>
2026-03-03 15:59:37 +0100

    Restore Azure root compatibility wrappers and root assets proxy

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+(function () {
+    var script = document.createElement('script');
+    script.src = '/public/assets/app.js?v=azure-proxy-1';
+    script.defer = true;
+    document.head.appendChild(script);
+})();
